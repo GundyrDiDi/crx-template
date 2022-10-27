c3a988a034122dfeb26c6394c71e6c1ba2ac78b1
shaozhilin <502121489@qq.com>
2022-10-27 20:37:52 +0800

    fix

--- src/service-worker/store.ts
+++ src/service-worker/store.ts
@@ -1,12 +1,11 @@
 const storage = chrome.storage.local
 
-console.log(ttttttt.8888)
-// const store = {}
+const store = {}
 
-// storage.get(store, res => {
-//   Object.keys(store).forEach(key => {
-//     if (!res[key]) {
-//       storage.set({ [key]: store[key] })
-//     }
-//   })
-// })
+storage.get(store, res => {
+  Object.keys(store).forEach(key => {
+    if (!res[key]) {
+      storage.set({ [key]: store[key] })
+    }
+  })
+})
